9d1c50022c4fee37c5ef73b7741a419d96da148b
dmcgaa <dmcgaa>
2019-12-08 15:29:25 +0000

    Heroku runs on different port, let process.env set PORT

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var io = require('socket.io')(http);
 
 app.use(express.static('dist'));
 
+app.set('port', (process.env.PORT || 8080));
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/dist/index.html');
 });
@@ -25,6 +27,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(8080, function(){
-  console.log('listening on *:8080');
+http.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
